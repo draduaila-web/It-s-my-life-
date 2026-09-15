331151815b79f80d64ae49e7942bd59c760419f3
draduaila-web <draduaila@gmail.com>
2026-09-15 20:38:02 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
-const CACHE = "bertha-v89";
+const CACHE = "bertha-v90";
 
 self.addEventListener("install", event => {
   event.waitUntil(self.skipWaiting());
