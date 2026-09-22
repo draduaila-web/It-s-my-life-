817ebe62b1d0bad3dc9aa641df497256164c3acf
draduaila-web <draduaila@gmail.com>
2026-09-22 17:30:58 -0300

    Add files via upload

--- homologacao/sw.js
+++ homologacao/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hml-bertha-v221-rc6-icon-library";
+const CACHE = "hml-bertha-v221-rc8-study-icons-fix";
 self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
 self.addEventListener("activate", event => event.waitUntil((async()=>{
   const keys=await caches.keys();
