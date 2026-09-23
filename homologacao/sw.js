cae83f89a0f898759f497d256cb15205a0335159
draduaila-web <draduaila@gmail.com>
2026-09-23 09:11:00 -0300

    Add files via upload

--- homologacao/sw.js
+++ homologacao/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hml-bertha-v221-rc30-finance-modal-system-hml";
+const CACHE = "hml-bertha-v221-rc31-finance-colors-hml";
 self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
 self.addEventListener("activate", event => event.waitUntil((async()=>{
   const keys=await caches.keys();
