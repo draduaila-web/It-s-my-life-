af419cea54d0edabb3c33896efd8b8284a93c272
draduaila-web <draduaila@gmail.com>
2026-09-23 13:33:33 -0300

    Add files via upload

--- homologacao/sw.js
+++ homologacao/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hml-bertha-v221-rc40-food-context-library-hml";
+const CACHE = "hml-bertha-v221-rc41-food-primary-context-hml";
 self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
 self.addEventListener("activate", event => event.waitUntil((async()=>{
   const keys=await caches.keys();
