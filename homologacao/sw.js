4abe9db4a830000e8f68a6696bc1e0df93c49752
draduaila-web <draduaila@gmail.com>
2026-09-24 15:31:14 -0300

    Add files via upload

--- homologacao/sw.js
+++ homologacao/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hml-bertha-v221-rc70-note-checkbox";
+const CACHE = "hml-bertha-v221-rc82-exercise-modal-proportion";
 self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
 self.addEventListener("activate", event => event.waitUntil((async()=>{
   const keys=await caches.keys();
