0bdf8046d9788d58c725185f52f14769355a8603
draduaila-web <draduaila@gmail.com>
2026-09-26 08:43:11 -0300

    Add files via upload

--- homologacao/sw.js
+++ homologacao/sw.js
@@ -1,7 +1,3 @@
 self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil((async()=>{
-  const ks=await caches.keys();
-  await Promise.all(ks.map(k=>caches.delete(k)));
-  await self.registration.unregister();
-})()));
+self.addEventListener('activate',e=>e.waitUntil((async()=>{const ks=await caches.keys();await Promise.all(ks.map(k=>caches.delete(k)));await self.registration.unregister();})()));
 self.addEventListener('fetch',()=>{});
