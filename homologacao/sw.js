a02f00c2e9b1e189fe5299bec2170c2d96e082f6
draduaila-web <draduaila@gmail.com>
2026-09-25 21:03:08 -0300

    Add files via upload

--- homologacao/sw.js
+++ homologacao/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hml-bertha-v221-rc147-kids-owner-helper";
+const CACHE = "hml-bertha-v221-rc148-hard-refresh";
 self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
 self.addEventListener("activate", event => event.waitUntil((async()=>{
   const keys=await caches.keys();
